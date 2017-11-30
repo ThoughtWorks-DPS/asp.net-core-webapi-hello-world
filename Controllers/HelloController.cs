5982953520780ba163b0651e672011bf565d43a3
Max Anderson <manderso@thoughtworks.com>
2017-11-30 10:46:02 -0500

    adding logging

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace HelloWorld.Controllers
 {
     [Route("api/[controller]")]
     public class HelloController : Controller
     {
+        ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/hello
         [HttpGet]
         public IDictionary<string,string> Get()
         {
-
+            using (_logger.BeginScope("a scope"))
+            {
+            _logger.LogInformation("we're going to say hello");
+            }
             return new Dictionary<string,string> {{ "message", "Hello, World!" }};
         }
 
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace HelloWorld
 {
@@ -14,12 +14,23 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .Build();
     }
 }
