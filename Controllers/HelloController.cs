66538c36e288b095f844fa089158615748bc5d8a
Max Anderson <manderso@thoughtworks.com>
2017-11-30 09:14:52 -0500

    initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        // GET api/hello
+        [HttpGet]
+        public IDictionary<string,string> Get()
+        {
+
+            return new Dictionary<string,string> {{ "message", "Hello, World!" }};
+        }
+
+    }
+}
